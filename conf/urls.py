fffa116b7971e7a91ad6616973cdc7f43196b300
max <iimax975ii@gmail.com>
2023-07-17 21:18:53 +0300

    add templates

--- conf/urls.py
+++ conf/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from project5.views import home_view, product_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
+    path('products', product_view),
+
 ]
--- project5/views.py
+++ project5/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from os import path
 # Create your views here.
+
+
+def home_view(request):
+    return render(request, "project5/index.html", context={})
+
+def product_view(request):
+    return render(request, "project5/products.html", context={})
+
+
